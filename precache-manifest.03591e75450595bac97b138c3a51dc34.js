1187df4e253a9234066d9eaa59fb65e2e962b953
Artem Kolinko <kalinrt@gmail.com>
2020-11-03 20:20:57 +0200

    Updates

---
+++ precache-manifest.03591e75450595bac97b138c3a51dc34.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea1b2ffd307d4c741580e0507312524",
+    "url": "/react-google-keeper/index.html"
+  },
+  {
+    "revision": "b2c30e89cef18b465fb3",
+    "url": "/react-google-keeper/static/js/2.49b598fc.chunk.js"
+  },
+  {
+    "revision": "c6edae0df82fd7aba205",
+    "url": "/react-google-keeper/static/js/main.990be6af.chunk.js"
+  },
+  {
+    "revision": "e2172a6f3c678165dd84",
+    "url": "/react-google-keeper/static/js/runtime-main.2cff4af6.js"
+  }
+]);
